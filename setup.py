432467c77444fa43be5d93d2aa941067f3114585
Tuan Nguyen <anhtuan.nguyen@me.com>
2021-07-24 09:04:27 +0700

    fix requirements

--- setup.py
+++ setup.py
@@ -6,14 +6,24 @@ with open('requirements.txt') as f:
 
 setup(
     name = 'dbt_generator',
-    version = '0.1.0',
+    version = '0.1.2',
     author = 'Tuan Nguyen',
-    author_email ='anhtuan.nguyen@me.com',
+    author_email = 'anhtuan.nguyen@me.com',
+    url = 'https://github.com/tuanchris/dbt-generator',
+    description = 'Generate and process base models for dbt',
+    license ='MIT',
     packages = find_packages(),
     entry_points = {
         'console_scripts': [
             'dbt-generator = dbt_generator.dbt_generator:dbt_generator',
         ]
     },
+    classifiers =(
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+    ),
+    keywords ='dbt python package base models',
     install_requires=requirements,
+    zip_safe = False
     )
