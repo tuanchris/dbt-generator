8a9277afa1226a10d2ea5280e1ad0ee4368906c6
Tuan Nguyen <anhtuan.nguyen@me.com>
2022-05-26 13:59:54 -0400

    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md') as f:
 
 setup(
     name = 'dbt_generator',
-    version = '0.3.1',
+    version = '0.3.2',
     author = 'Tuan Nguyen',
     author_email = 'anhtuan.nguyen@me.com',
     url = 'https://github.com/tuanchris/dbt-generator',
